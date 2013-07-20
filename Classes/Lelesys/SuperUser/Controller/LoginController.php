aa4e8b1d9d5669ce8307f9f210fc82226d8c9d32
Sushant Chari <sushant.chari@lelesys.com>
2013-07-20 12:50:29 +0530

    [TASK] Super user functionality for multi user group
    Super user functionality shpould work for more than one user group
    
    Fixes/relates:
    Sprint: 29

--- Classes/Lelesys/SuperUser/Controller/LoginController.php
+++ Classes/Lelesys/SuperUser/Controller/LoginController.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace Lelesys\SuperUser\Controller;
 
-/*                                                                        *
+/* *
  * This script belongs to the TYPO3 Flow package "Lelesys.SuperUser".     *
  *                                                                        *
  *                                                                        */
@@ -24,36 +25,50 @@ class LoginController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	protected $superUserService;
 
 	/**
+	 * The security conntext
+	 *
+	 * @var \TYPO3\Flow\Security\Context
+	 * @Flow\Inject
+	 */
+	protected $securityContext;
+
+	/**
 	 * Superuser can login as other user ang get redirected to Users home page.
 	 *
 	 * @param \TYPO3\Flow\Security\Account $account
 	 * @return void
 	 */
 	public function loginAsUserAction(\TYPO3\Flow\Security\Account $account) {
-		if (empty($this->settings['superUserRedirectOptions']['loginAsUserFailed']['package'])
-			|| empty($this->settings['superUserRedirectOptions']['loginAsUserFailed']['controller'])
-			|| empty($this->settings['superUserRedirectOptions']['loginAsUserFailed']['action'])) {
-				throw new \Lelesys\SuperUser\Exception('Superuser redirect options for login as user failed are not set', 1371037253);
+		$roles = $account->getRoles();
+		foreach ($roles as $role) {
+			$loginUserRole = $role->__toString();
+			break;
 		}
+		$userRoles = $this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'];
+		foreach ($userRoles as $key => $userRole) {
+			if ($loginUserRole === $key) {
+				if (empty($this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'][$key]['package'])
+						|| empty($this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'][$key]['controller'])
+						|| empty($this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'][$key]['action'])) {
+					throw new \Lelesys\SuperUser\Exception('Superuser redirect options for login as user failed are not set', 1371037253);
+				}
 
-		if (empty($this->settings['superUserRedirectOptions']['loginAsUserSuccess']['package'])
-			|| empty($this->settings['superUserRedirectOptions']['loginAsUserSuccess']['controller'])
-			|| empty($this->settings['superUserRedirectOptions']['loginAsUserSuccess']['action'])) {
-				throw new \Lelesys\SuperUser\Exception('Superuser redirect options for login as user success are not set', 1371037260);
-		}
+				if (empty($this->settings['superUserRedirectOptions']['loginAsUserSuccess']['roles'][$key]['package'])
+						|| empty($this->settings['superUserRedirectOptions']['loginAsUserSuccess']['roles'][$key]['controller'])
+						|| empty($this->settings['superUserRedirectOptions']['loginAsUserSuccess']['roles'][$key]['action'])) {
+					throw new \Lelesys\SuperUser\Exception('Superuser redirect options for login as user success are not set', 1371037260);
+				}
 
-		if ($this->superUserService->loginAsUser($account)) {
-			$this->redirect(
-					$this->settings['superUserRedirectOptions']['loginAsUserSuccess']['action'],
-					$this->settings['superUserRedirectOptions']['loginAsUserSuccess']['controller'],
-					$this->settings['superUserRedirectOptions']['loginAsUserSuccess']['package']
-				);
-		} else {
-			$this->redirect(
-					$this->settings['superUserRedirectOptions']['loginAsUserFailed']['action'],
-					$this->settings['superUserRedirectOptions']['loginAsUserFailed']['controller'],
-					$this->settings['superUserRedirectOptions']['loginAsUserFailed']['package']
-				);
+				if ($this->superUserService->loginAsUser($account)) {
+					$this->redirect(
+							$this->settings['superUserRedirectOptions']['loginAsUserSuccess']['roles'][$key]['action'], $this->settings['superUserRedirectOptions']['loginAsUserSuccess']['roles'][$key]['controller'], $this->settings['superUserRedirectOptions']['loginAsUserSuccess']['roles'][$key]['package']
+					);
+				} else {
+					$this->redirect(
+							$this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'][$key]['action'], $this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'][$key]['controller'], $this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'][$key]['package']
+					);
+				}
+			}
 		}
 	}
 
@@ -63,18 +78,26 @@ class LoginController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 * @return void
 	 */
 	public function logoutAsUserAction() {
-		if (empty($this->settings['superUserRedirectOptions']['logoutAsUser']['package'])
-			|| empty($this->settings['superUserRedirectOptions']['logoutAsUser']['controller'])
-			|| empty($this->settings['superUserRedirectOptions']['logoutAsUser']['action'])) {
-				throw new \Lelesys\SuperUser\Exception('Superuser redirect options for logout as user are not set', 1371037270);
+		$roles = $this->securityContext->getAccount()->getRoles();
+		foreach ($roles as $role) {
+			$loginUserRole = $role->__toString();
+			break;
 		}
+		$userRoles = $this->settings['superUserRedirectOptions']['loginAsUserFailed']['roles'];
+		foreach ($userRoles as $key => $userRole) {
+			if ($loginUserRole === $key) {
+				if (empty($this->settings['superUserRedirectOptions']['logoutAsUser']['roles'][$key]['package'])
+						|| empty($this->settings['superUserRedirectOptions']['logoutAsUser']['roles'][$key]['controller'])
+						|| empty($this->settings['superUserRedirectOptions']['logoutAsUser']['roles'][$key]['action'])) {
+					throw new \Lelesys\SuperUser\Exception('Superuser redirect options for logout as user are not set', 1371037270);
+				}
 
-		$this->superUserService->logoutAsUser();
-		$this->redirect(
-					$this->settings['superUserRedirectOptions']['logoutAsUser']['action'],
-					$this->settings['superUserRedirectOptions']['logoutAsUser']['controller'],
-					$this->settings['superUserRedirectOptions']['logoutAsUser']['package']
+				$this->superUserService->logoutAsUser();
+				$this->redirect(
+						$this->settings['superUserRedirectOptions']['logoutAsUser']['roles'][$key]['action'], $this->settings['superUserRedirectOptions']['logoutAsUser']['roles'][$key]['controller'], $this->settings['superUserRedirectOptions']['logoutAsUser']['roles'][$key]['package']
 				);
+			}
+		}
 	}
 
 }
